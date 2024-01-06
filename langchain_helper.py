e29493ccc2856abdc4ff048eaff23a6348fc70a5
Gaurav Bhattacharjee <grvb1515@gmail.com>
2024-01-06 17:48:27 +0530

    Add prompt chain and streamlit frontend

---
+++ langchain_helper.py
@@ -0,0 +1,80 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from secret_key import openapi_key
+
+import os
+os.environ['OPENAI_API_KEY'] = openapi_key
+
+llm = OpenAI(temperature=0.8, max_tokens=-1)
+
+def generate_rubic(job_role, skills,job_complexity, expectations, organization_type, client_facing, mission_critical, impact_of_design):
+    prompt_template_name = PromptTemplate(
+        input_variables=['job_role', 'skills','job_complexity', 'expectations', 'organization_type', 'client_facing', 'mission_critical', 'impact_of_design'],
+        template="""
+        Pretend that you are a talent-acquisition correspondent of a reputed firm. 
+        Given a job role, hiring context and skills list, you have to create a detailed job description.
+        Do not mention the hiring context or its title in the resume, but include its contents implicitly in the JD.
+        Can you create one for the following:
+        JOB ROLE: {job_role}
+        SKILLS: {skills}
+        HIRING CONTEXT: 
+        - Job Complexity - {job_complexity}
+        - Application Performance Expectations - {expectations}
+        - Organization Type - {organization_type}
+        - Client Facing - {client_facing}
+        - Mission Critical - {mission_critical}
+        - Impact of Design - {impact_of_design}
+        Use the following JD for reference:
+        Job Description: Frontend Engineer (UI/UX)
+
+        Are you passionate about crafting beautiful and intuitive web UIs? Do you thrive on turning complex datasets into engaging visualizations? Join our mission to revolutionize Dev & Ops by creating impactful UIs that empower users to explore data effortlessly.
+
+        Responsibilities:
+
+        Transform large datasets into visually compelling and usable UIs using React and Redux.
+        Collaborate with a close-knit team to solve challenging problems and improve performance.
+        Take ownership of meaningful parts of our service, making a significant impact on our projects.
+        Requirements:
+
+        Several years of experience designing and engineering UIs.
+        Mastery of Javascript, HTML, and CSS with awareness of the latest frontend technologies.
+        Proficiency in using design patterns for creating simple and reusable components.
+        Familiarity with data structures, algorithms, profiling, and optimization.
+        Visible online work or portfolio showcasing your skills.
+        Appreciation for code simplicity, performance, and attention to detail.
+        Desire to work in a fast-paced, high-growth startup environment.
+        Bonus Points:
+
+        Experience with D3.
+        Previous work on large applications using React, Angular, Backbone, Ember, Flux, or Redux.
+        Comfort with ES6 and Babel for Javascript.
+        Enjoyment of writing in Python, Ruby, C++, Rust, or Go.
+        If you're excited about transforming complex data into stunning UIs and want to be part of a dynamic startup environment, we invite you to apply and contribute to the future of Dev & Ops.
+        """
+    )
+
+    jd_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key='job_description')
+
+    prompt_template_items = PromptTemplate(
+        input_variables=['job_description'],
+        template="""
+        Pretend that you are an interviewer. 
+        Suggest interview rubrics for the job description: 
+        {job_description}
+        Present your findings in the form of a spreadsheet.
+        """
+    )
+
+    rubric_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="rubric")
+
+    chain = SequentialChain(
+        chains=[jd_chain, rubric_chain],
+        input_variables=['job_role', 'skills','job_complexity', 'expectations', 'organization_type', 'client_facing', 'mission_critical', 'impact_of_design'],
+        output_variables=['job_description', 'rubric']
+    )
+
+    response = chain({'job_role': job_role, 'skills': skills,'job_complexity':job_complexity, 'expectations':expectations, 'organization_type':organization_type, 'client_facing':client_facing, 'mission_critical':mission_critical, 'impact_of_design':impact_of_design})
+    return response
+
---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import langchain_helper
+
+st.title("Taptic Interview Assistant")
+
+st.subheader("Job Role")
+job_role = st.text_input("Specify job role here (ex: Backend Developer)")
+
+st.subheader("Hiring Context")
+job_complexity = st.selectbox("Job Complexity:", ["Low", "Medium", "High"])
+expectations = st.selectbox("Performance Expectations:", ["Low", "Medium", "High"])
+organization_type = st.selectbox("Organization Type:", ["SI", "GCC", "Product", "Startup"])
+client_facing = st.selectbox("Is the role client facing?:", ["Yes", "No"])
+mission_critical = st.selectbox("Mission critical/Hiring for immediate joiners?:", ["Yes", "No"])
+impact_of_design = st.selectbox("Impact of design:", ["Low", "Medium", "High"])
+
+st.subheader("YOE")
+yoe = st.number_input("Specify desired YOE (ex: 0-3 years)", step=1, format="%d")
+
+st.subheader("Skills")
+skills = st.text_input("Specify list of desired skills - you can seperate the skill items using commas")
+
+
+generate_rubric = st.button("Generate rubric")
+
+# Check if the button is clicked
+if generate_rubric:
+    if (job_role and yoe and skills):
+        response = langchain_helper.generate_rubic(job_role, skills,job_complexity, expectations, organization_type, client_facing, mission_critical, impact_of_design)
+        # print(response)
+        st.header('JD')
+        st.write(response['job_description'])
+    else:
+        st.write("Please do not leave any field empty. All fields are mandatory.")
+
+
